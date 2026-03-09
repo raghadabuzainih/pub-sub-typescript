f7cec19a49729f3a2bda299eb7d6c1d5cbac3163
raghadabuzainih <raghad.abuzainih@gmail.com>
2026-03-09 23:54:04 +0200

    set REPL commands on server side

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,7 @@
 import amqp from 'amqplib'
 import { publishJSON } from '../internal/pubsub.js'
 import { ExchangePerilDirect, PauseKey } from '../internal/routing/routing.js'
+import { getInput, printServerHelp } from '../internal/gamelogic/gamelogic.js'
 
 async function main() {
   const rabbitConnString = 'amqp://guest:guest@localhost:5672/'
@@ -11,6 +12,23 @@ async function main() {
   const channel = await conn.createConfirmChannel()
   //publish msg from this channel
   publishJSON(channel, ExchangePerilDirect, PauseKey, {isPaused: true})
+
+  printServerHelp()
+  while(true){
+    const words = await getInput()
+    if(words.length > 0){
+      if(words[0] === 'pause'){
+        console.log('i am sending a pause message')
+        publishJSON(channel, ExchangePerilDirect, PauseKey, {isPaused: true})
+      }else if(words[0] === 'resume'){
+        console.log('i am sending a resume message')
+        publishJSON(channel, ExchangePerilDirect, PauseKey, {isPaused: false})
+      }else if(words[0] === 'quit'){
+        console.log('i am exiting')
+        break
+      }else console.log("i don't understand the command")
+    }
+  }
 
   process.on('exit', async()=>{
     console.log('The program is shutting down')
