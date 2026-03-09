83561cb3fc3bb557fc1b1416c0647c5e82350781
raghadabuzainih <raghad.abuzainih@gmail.com>
2026-03-09 23:07:10 +0200

    connect RabbitMQ with client, declare queue & bind it with exchange

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,5 +1,14 @@
+import amqp from 'amqplib'
+import { clientWelcome } from '../internal/gamelogic/gamelogic.js'
+import { declareAndBind, SimpleQueueType } from '../internal/pubsub.js'
+import { ExchangePerilDirect, PauseKey } from '../internal/routing/routing.js'
+
 async function main() {
-  console.log("Starting Peril client...");
+  const rabbitConnString = 'amqp://guest:guest@localhost:5672/'
+    const conn = await amqp.connect(rabbitConnString)
+    console.log("connection was successful.")
+    const username= await clientWelcome()
+    declareAndBind(conn, ExchangePerilDirect, `pause.${username}`, PauseKey, SimpleQueueType.Transient)
 }
 
 main().catch((err) => {
--- src/internal/pubsub.ts
+++ src/internal/pubsub.ts
@@ -1,4 +1,5 @@
-import type { ConfirmChannel } from "amqplib";
+import type { Channel, ConfirmChannel } from "amqplib";
+import amqp from 'amqplib'
 
 export function publishJSON<T>(
   ch: ConfirmChannel,
@@ -9,4 +10,29 @@ export function publishJSON<T>(
     const json = JSON.stringify(value)
     const buffer = Buffer.from(json)
     ch.publish(exchange,routingKey, buffer, {contentType: 'application/json'})
+}
+
+export enum SimpleQueueType {
+  Durable,
+  Transient,
+}
+
+export async function declareAndBind(
+  conn: amqp.ChannelModel,
+  exchange: string,
+  queueName: string,
+  key: string,
+  queueType: SimpleQueueType,
+): Promise<[Channel, amqp.Replies.AssertQueue]>{
+
+    const channel = await conn.createChannel()
+    const queue = await channel.assertQueue(queueName, {
+        durable: queueType === SimpleQueueType.Durable ? true : false,
+        autoDelete: queueType === SimpleQueueType.Transient ? true : false,
+        exclusive: queueType === SimpleQueueType.Transient ? true : false,
+    })
+
+    await channel.bindQueue(queue.queue, exchange, key)
+    
+    return [channel, queue]
 }
